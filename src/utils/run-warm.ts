125ff5ff83140218c9c84462bf1989b57db19b16
Kolya Venturi <me@kolya.co>
2021-07-10 10:26:27 -0700

    Fix linting issues

--- src/utils/run-warm.ts
+++ src/utils/run-warm.ts
@@ -1,11 +1,17 @@
-type LambdaFn = (event: any, context: any, callback: any) => any;
+type Event = AWSLambda.APIGatewayEvent & {
+  source?: string;
+};
+type LambdaFn = (event: AWSLambda.APIGatewayEvent, context: any, callback: any) => any;
 
-const runWarm = (lambdaFunc: LambdaFn): AWSLambda.Handler => (event, context, cb): any => {
+const runWarm = (lambdaFunc: LambdaFn): AWSLambda.Handler => (event: Event, context, cb) => {
   if (event.source === 'serverless-plugin-warmup') {
     console.log('Function warmed up.');
     return 'pinged';
   }
 
+  /* eslint-disable-next-line no-warning-comments */
+  // TODO: Type this better
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-return */
   return lambdaFunc(event, context, cb);
 };
 
